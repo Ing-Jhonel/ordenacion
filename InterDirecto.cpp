dc40a8aff7db694fcdaa5662cf93501fce2b5bdd
Jhonel Apaza <jgapazap@unjbg.edu.pe>
2024-09-13 14:37:59 -0500

    Agregue gitignore .exe

---
+++ InterDirecto.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	
+	int n, num;
+	
+	cout << "Indique la cantidad de elementos del arreglo: ";
+	cin >> n;
+	cout << endl;
+	int numeros[n];
+
+	for(int i=0; i<n; i++){
+		cout << i+1 << ". elemento: ";
+		cin >> num;
+	}
+	
+	cout << endl << endl << endl;
+	
+	char opcion;
+	cout << "Indique el metodo de intercambio: " << endl << endl;
+	cout << "a) Intercambio directo por la derecha." << endl;
+	cout << "b) Intercambio directo por la izquierda." << endl;
+	cout << "c) Intercambio directo con senial." << endl;
+	cout << "d) Intercambio directo bidireccional." << endl;
+	cout << "e) Salir." << endl << endl;
+	
+	cin >> opcion;
+	
+	switch (opcion){
+		
+		case 'a':
+			
+			break;
+		case 'b':
+			break;
+		case 'c':
+			break;
+		case 'd':
+			break;
+		case 'e':
+			break;
+	}
+	return 0;
+}
