d1170a607a72aa24d05d0331af1f3bd4f09e1eac
Jhonel Apaza <jgapazap@unjbg.edu.pe>
2024-09-13 14:41:07 -0500

    Termine interfaz baqsica del programa

--- InterDirecto.cpp
+++ InterDirecto.cpp
@@ -8,6 +8,7 @@ int main(){
 	cout << "Indique la cantidad de elementos del arreglo: ";
 	cin >> n;
 	cout << endl;
+	
 	int numeros[n];
 
 	for(int i=0; i<n; i++){
@@ -18,28 +19,35 @@ int main(){
 	cout << endl << endl << endl;
 	
 	char opcion;
-	cout << "Indique el metodo de intercambio: " << endl << endl;
-	cout << "a) Intercambio directo por la derecha." << endl;
-	cout << "b) Intercambio directo por la izquierda." << endl;
-	cout << "c) Intercambio directo con senial." << endl;
-	cout << "d) Intercambio directo bidireccional." << endl;
-	cout << "e) Salir." << endl << endl;
-	
-	cin >> opcion;
-	
-	switch (opcion){
+	do{
+		cout << "Indique el metodo de intercambio: " << endl << endl;
+		cout << "a) Intercambio directo por la derecha." << endl;
+		cout << "b) Intercambio directo por la izquierda." << endl;
+		cout << "c) Intercambio directo con senial." << endl;
+		cout << "d) Intercambio directo bidireccional." << endl;
+		cout << "e) Salir." << endl << endl;
 		
-		case 'a':
+		cin >> opcion;
+		
+		cout << endl << endl;
+		
+		switch (opcion){
 			
-			break;
-		case 'b':
-			break;
-		case 'c':
-			break;
-		case 'd':
-			break;
-		case 'e':
-			break;
-	}
+			case 'a':
+				
+				break;
+			case 'b':
+				break;
+			case 'c':
+				break;
+			case 'd':
+				break;
+			case 'e':
+				cout << "Saliendo del programa...";
+				break;
+		}
+		cout << endl << endl;
+	} while (opcion != 'e');
+	
 	return 0;
 }
