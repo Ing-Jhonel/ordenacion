bcdee76a78749651f9eb848b734dbe5a5153d656
Jhonel <jgapazap@unjbg.edu.pe>
2024-09-25 07:27:34 -0500

    Insercciones directa y binaria

--- inserciones.cpp
+++ inserciones.cpp
@@ -1,7 +1,10 @@
 #include <iostream>
 using namespace std;
 
-void seleccionDirecta(double arr[], int n);
+void InsercionDir(double arr[], int n);
+void InsercionBin(double arr[], int n);
+void imprimir(double arr[], int n);
+void change(double a[], double b[], int n);
 
 int main(){
 	
@@ -9,40 +12,66 @@ int main(){
 	cout << "La cantidad de elementos a ingresar: ";
 	cin >> tamanio;
 	double arreglo[tamanio];
-	
+	double copia[tamanio];
 	for(int i=0; i<tamanio; i++){
 		cout << i+1 << ". elemento: ";
 		cin >> arreglo[i];
+		copia[i]=arreglo[i];
 	}
 	char opcion;
-	cout << endl << "a) Insercion directa." << endl << "b) Insercio binaria" << endl;
-	cin >> opcion;
-	
-	switch(opcion){
+	do{
+		cout << endl << "a) Insercion directa." << endl << "b) Insercio binaria" << endl << "c) Salir" << endl << endl;
+		cin >> opcion;
 		
-		case 'a':
-			InsercionDir(arreglo, tamanio);
-			break;
-		case 'b':
-			InsercionBin(arreglo, tamanio);
-			break;
-		default:
-			cout << "Indique una opcion valida.";
-			break;
-	}
+		cout << endl << endl;
+		switch(opcion){
+			case 'a':
+				InsercionDir(copia, tamanio);
+				cout << "Antes: "; imprimir(arreglo, tamanio); cout << endl;
+				cout << "Despues: "; imprimir(copia, tamanio);
+				break;
+			case 'b':
+				InsercionBin(copia, tamanio);
+				cout  << "Antes: "; imprimir(arreglo, tamanio); cout << endl;
+				cout << "Despues: "; imprimir(copia, tamanio);
+				break;
+			case 'c':
+				cout << "Saliendo...";
+				break;
+			default:
+				cout << "Indique una opcion valida.";
+				break;
+		}
+		cout << endl << endl;
+		change(copia, arreglo, tamanio);
+	}while(opcion!='c');
 	cout << endl;
 	return 0;
 }
 
+void change(double a[], double b[], int n){
+
+	for(int i=0; i<n; i++){
+		a[i]=b[i];
+	}
+	
+}
+void imprimir(double a[], int n){
+	
+	for(int i=0; i<n; i++){
+		cout << a[i] << " ";
+	}
+}
+
 void InsercionDir(double a[], int n){
 	
-	for(int i=2; i<n; i++){
-		int aux = a[i];
+	for(int i=1; i<n; i++){
+		double aux = a[i];
 		int k = i-1;
 		
-		while(k>0 && aux<a[k]){
+		while(k>=0 && aux<a[k]){
 			a[k+1] = a[k];
-			k = k+1;
+			k--;
 		}
 		a[k+1] = aux;
 	}
@@ -50,9 +79,9 @@ void InsercionDir(double a[], int n){
 
 void InsercionBin(double a[], int n){
 	
-	for(int i=2; i<n; i++){
-		int aux = a[i];
-		int izq = 1;
+	for(int i=1; i<n; i++){
+		double aux = a[i];
+		int izq = 0;
 		int der = i-1;
 		
 		while(izq <= der){
@@ -67,7 +96,7 @@ void InsercionBin(double a[], int n){
 		
 		while(j>=izq){
 			a[j+1] = a[j];
-			j = j-1;
+			j--;
 		}
 		a[izq] = aux;
 	}
